de12ce347f6db32e52b8bdfc0255135a5a7f3538
Amira Tarek <amirat737@gmail.com>
2024-05-09 16:42:35 +0300

    firebase is ready

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,16 @@ import 'package:chat_app/utils/constants.dart';
 import 'package:chat_app/views/login_view.dart';
 import 'package:chat_app/views/signup_view.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  // main is async => binding must be initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ChatApp());
 }
 
