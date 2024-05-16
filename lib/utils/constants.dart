353bf1fcbf1e8ece7d88f961f2c8d28a027591cb
Amira Tarek <amirat737@gmail.com>
2024-05-16 20:34:03 +0300

    edit bubble color

--- lib/utils/constants.dart
+++ lib/utils/constants.dart
@@ -5,8 +5,8 @@ const kWhite = Colors.white;
 const kPrimaryColor = Color(0xff17212B);
 const kSecondryColor = Color(0xff3F96D0);
 const kChatColor = Color(0xff0E1621);
-const kSentMessageColor = Color(0xff182533);
-const kRecievedMessageColor = Color(0xff2B5278);
+const kRecievedMessageColor = Color(0xff182533);
+const kSentMessageColor = Color(0xff2B5278);
 
 const kLogo = 'assets/images/chat.png';
 const kMessageCollection = 'messages';
@@ -14,14 +14,13 @@ const kMessage = 'message';
 const kTime = 'time';
 const kEmail = 'email';
 
-
 const kChatRoute = 'Chat View';
 const kSignupRoute = 'Signup View';
 const kLoginRoute = 'Login View';
 
 const outlineInputBorder = OutlineInputBorder(
-      borderSide: BorderSide.none,
-      borderRadius: BorderRadius.all(
-        Radius.circular(0),
-      ),
-    );
+  borderSide: BorderSide.none,
+  borderRadius: BorderRadius.all(
+    Radius.circular(0),
+  ),
+);
